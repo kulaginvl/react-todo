877cc8d1c19e11394a33fa78965b297c90aa828b
kulaginvl <vladkulagin0801@gmail.com>
2021-05-11 19:56:16 +0300

    Refact in func

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import './Modal.css';
 
-export default class Modal extends React.Component {
-  state = {
-    isOpen: false,
-  };
-  render() {
-    return (
-      <React.Fragment>
-        <button onClick={() => this.setState({ isOpen: true })}>Open M</button>
+function Modal() {
+  const [isOpen, setOpen] = React.useState(false);
 
-        {this.state.isOpen && (
-          <div className="modal">
-            <div className="modal_body">
-              <h1>Modal title</h1>
-              <p> Can't stop Wont't stop</p>
-              <button onClick={() => this.setState({ isOpen: false })}>Close M</button>
-            </div>
+  const toggleOpen = () => {
+    setOpen({ isOpen: true });
+  };
+  const toggleClose = () => {
+    setOpen((isOpen) => !isOpen);
+  };
+
+  return (
+    <React.Fragment>
+      <button onClick={toggleOpen}>Open M</button>
+
+      {isOpen && (
+        <div className="modal">
+          <div className="modal_body">
+            <h1>Modal title</h1>
+            <p> Can't stop Wont't stop</p>
+            <button onClick={toggleClose}>Close M</button>
           </div>
-        )}
-      </React.Fragment>
-    );
-  }
+        </div>
+      )}
+    </React.Fragment>
+  );
 }
+export default Modal;
